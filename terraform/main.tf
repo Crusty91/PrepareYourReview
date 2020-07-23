08ee72838f3de601a46807db3f08046171330a73
Sylvain Barbot <sylvain.barbot@edgenda.com>
2020-07-22 21:38:52 -0400

    fixes script tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -399,5 +399,5 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = aws_lambda_function.example_test_function.function_name
   principal     = "apigateway.amazonaws.com"
 
-   source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.example_api.id}/*/${aws_api_gateway_method.example_api_method.http_method}${aws_api_gateway_resource.example_api_resource.path}"
+   source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.apig.id}/*/${aws_api_gateway_method.apig_method.http_method}${aws_api_gateway_resource.apig_resource.path}"
 }
